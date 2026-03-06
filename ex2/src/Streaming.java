a3dc748c4c63d2764877c3d605bd1dbd2f491601
Vinicius <viniciusdss2004@gmail.com>
2026-03-06 20:16:51 -0300

    Atualização ex2

--- ex2/src/Streaming.java
+++ ex2/src/Streaming.java
@@ -1,17 +1,18 @@
 public class Streaming {
-    public String usuario, plano, ultimoFilmeAssistido; 
-    public float mensalidade; 
+
+    public String usuario, plano, ultimoFilmeAssistido;
+    public float mensalidade;
     public boolean ativo;
 
-    public Streaming(){
+    public Streaming() {
 
     }
 
-    public Streaming(String usuario, String plano) {
+    public Streaming(String usuario, String plano, String ultimoFilmeAssistido) {
         this.usuario = usuario;
         this.plano = plano;
 
-         // Define mensalidade conforme o plano
+        // Define mensalidade conforme o plano
         if (plano.equalsIgnoreCase("Básico")) {
             this.mensalidade = 25.90f;
         } else if (plano.equalsIgnoreCase("Premium")) {
@@ -23,27 +24,29 @@ public class Streaming {
         this.ativo = true;
         this.ultimoFilmeAssistido = "";
     }
-    //assistir o filme
-    public assistirFilme(String nomeFilme){
-         if (this.ativo) {
+
+    // Assistir filme
+    public void assistirFilme(String nomeFilme) {
+        if (this.ativo) {
             this.ultimoFilmeAssistido = nomeFilme;
             System.out.println("Assistindo: " + nomeFilme);
         } else {
             System.out.println("Conta inativa. É necessário pagar a fatura.");
-        }   
-    }
-    //consultar conta
-    public cancelarAssinatura(){
-            this.plano = false;
-
-    }   
-    //
-    public String toString(){
-        return " Usuário: " + this.usuario +
-                " Plano: " + this.plano +
-                " Mensalidade: " + this.mensalidade +
-                " Ultimo filme assistido: " + this.ultimoFilmeAssistido +
-                " Status da Conta : " + (this.ativo ? "ativo": "suspenso");
+        }
     }
 
+    // Cancelar assinatura
+    public void cancelarAssinatura() {
+        this.ativo = false;
+        System.out.println("Assinatura cancelada.");
+    }
+
+    // Mostrar dados
+    public String toString() {
+        return "Usuário: " + this.usuario +
+                "\nPlano: " + this.plano +
+                "\nMensalidade: " + this.mensalidade +
+                "\nÚltimo filme assistido: " + this.ultimoFilmeAssistido +
+                "\nStatus da conta: " + (this.ativo ? "Ativo" : "Suspenso");
+    }
 }
--- ex2/src/TesteSreaming.java
+++ ex2/src/TesteSreaming.java
@@ -1,7 +1,8 @@
 public class TesteSreaming{
     public static void main(String[] args) throws Exception {
 
-        Streaming us1 = new Streaming("Vinicius", "Básico");
+        Streaming us1 = new Streaming("Vinicius", "Básico","Shrek 2");
+        System.out.println(us1.toString());
 
     }
 }
